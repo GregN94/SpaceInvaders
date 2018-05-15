be0ce625317cdc2f31de4ccf9b568e92f211b73f
GregN94 <grzesiekn1001@gmail.com>
2018-05-15 13:41:43 +0200

    Initial position for Player

--- main.py
+++ main.py
@@ -1,41 +1,32 @@
 import pygame
 from player import Player
 
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-BLUE = (0, 128, 255)
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+QUIT = False
 
 pygame.init()
-screen = pygame.display.set_mode((400, 300))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-done = False
-
-player = Player()
+player = Player(int(SCREEN_WIDTH / 2), SCREEN_HEIGHT)
 
 all_sprites_list = pygame.sprite.Group()
-
 all_sprites_list.add(player)
 
 clock = pygame.time.Clock()
 
-while not done:
+while not QUIT:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            done = True
-        # if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-        #    is_blue = not is_blue
+            QUIT = True
 
     pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_UP]: player.rect.y -= 3
-    if pressed[pygame.K_DOWN]: player.rect.y += 3
-    if pressed[pygame.K_LEFT]: player.rect.x -= 3
-    if pressed[pygame.K_RIGHT]: player.rect.x += 3
+    if pressed[pygame.K_UP]: player.move_up()
+    if pressed[pygame.K_DOWN]: player.move_down()
+    if pressed[pygame.K_LEFT]: player.move_left()
+    if pressed[pygame.K_RIGHT]: player.move_rght()
 
-    screen.fill(BLACK)
-    color = (0, 128, 255)
+    screen.fill((0, 0, 0))
     all_sprites_list.draw(screen)
-
-
-
     pygame.display.update()
     clock.tick(60)
--- player.py
+++ player.py
@@ -1,11 +1,27 @@
 import pygame
-SCALE = 4
+
+SCALE = 5
+POSITION_OFFSET = 10
+SPEED = 5
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x, y):
         super().__init__()
 
         self.image = pygame.image.load("Images/space_ship.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, [int(dimension / SCALE) for dimension in self.image.get_size()])
         self.rect = self.image.get_rect()
+        self.rect.center = (x, y - int(self.image.get_height() / 2) - POSITION_OFFSET)
+
+    def move_left(self):
+        self.rect.x -= SPEED
+
+    def move_rght(self):
+        self.rect.x += SPEED
+
+    def move_up(self):
+        self.rect.y -= SPEED
+
+    def move_down(self):
+        self.rect.y += SPEED
