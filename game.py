3084605cec676a7dbd3a35d7af03887d9c4df660
GregN94 <grzesiekn1001@gmail.com>
2018-05-25 16:12:34 +0200

    Menu Refactor

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import pygame
 from play_state import PlayState
-from menu_state import MenuState
+from menu_state import MenuState, States
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
@@ -15,7 +15,6 @@ class Game:
         self.play_state = PlayState(SCREEN_WIDTH, SCREEN_HEIGHT)
         self.play_state.generate_enemies()
         self.menu_state = MenuState(SCREEN_WIDTH, SCREEN_HEIGHT)
-        self.start = False
 
     def check_if_game_quited(self):
         for event in pygame.event.get():
@@ -23,17 +22,18 @@ class Game:
                 self.QUIT = True
 
     def main(self):
+        state = States.MENU
         while not self.QUIT:
 
             self.screen.fill((0, 0, 0))
             self.check_if_game_quited()
 
-            if self.start:
+            if state == States.GAME:
                 self.play_state.play()
                 self.play_state.draw(self.screen)
-            else:
+            if state == States.MENU:
                 self.menu_state.draw(self.screen)
-                self.start = self.menu_state.menu()
+                state = self.menu_state.menu()
             pygame.display.update()
             self.clock.tick(60)
 
--- menu_state.py
+++ menu_state.py
@@ -1,8 +1,15 @@
 import pygame
+from enum import Enum
 from menu_sprites import MenuSprite, LogoSprite, StartButtonSprite
 
 INACTIVE = 0
 ACTIVE = 1
+
+
+class States(Enum):
+    GAME = 1
+    MENU = 2
+    EXIT = 3
 
 
 class MenuState:
@@ -15,17 +22,18 @@ class MenuState:
         self.menu_sprites_list.add(self.menu_sprite)
         self.menu_sprites_list.add(self.logo_sprite)
         self.menu_sprites_list.add(self.start_button_sprite)
+        self.new_state = States.MENU
 
     def menu(self):
-        self.start_button_sprite.set_state(INACTIVE)
         mouse_position = pygame.mouse.get_pos()
+        self.start_button_sprite.set_state(INACTIVE)
         if self.start_button_sprite.rect.collidepoint(mouse_position):
             self.start_button_sprite.set_state(ACTIVE)
             pressed = pygame.mouse.get_pressed()
             if pressed[0]:
-                return True
-            else:
-                return False
+                self.new_state = States.GAME
+
+        return self.new_state
 
     def draw(self, screen):
         self.menu_sprites_list.draw(screen)
