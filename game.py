a054f468b323abddbde9dba37fca318f8fc8dbd1
GregN94 <grzesiekn1001@gmail.com>
2018-05-29 15:24:09 +0200

    Adding exit button to pause

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ SCREEN_HEIGHT = 600
 class Game:
     def __init__(self):
         pygame.init()
-        self.QUIT = False
+        self.EXIT = False
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.play_state = PlayState(SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -19,10 +19,10 @@ class Game:
         self.pause_state = Pause(SCREEN_WIDTH, SCREEN_HEIGHT)
         self.state = States.MENU
 
-    def execute_basic_input(self):
+    def basic_input(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.QUIT = True
+                self.EXIT = True
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_ESCAPE:
                     if self.state == States.PAUSE:
@@ -30,24 +30,37 @@ class Game:
                     elif self.state == States.GAME:
                         self.state = States.PAUSE
 
+    def state_pause(self):
+        self.play_state.draw(self.screen)
+        self.state = self.pause_state.update()
+        self.pause_state.draw(self.screen)
+
+    def state_game(self):
+        self.play_state.play()
+        self.play_state.draw(self.screen)
+
+    def state_menu(self):
+        self.state = self.menu_state.menu()
+        self.menu_state.draw(self.screen)
+
     def main(self):
-        while not self.QUIT:
+
+        while not self.EXIT:
+
             self.screen.fill((0, 0, 0))
-            self.execute_basic_input()
+            self.basic_input()
 
             if self.state == States.PAUSE:
-                self.play_state.draw(self.screen)
-                self.pause_state.draw(self.screen)
+                self.state_pause()
 
             if self.state == States.GAME:
-                self.play_state.play()
-                self.play_state.draw(self.screen)
+                self.state_game()
 
             if self.state == States.MENU:
-                self.state = self.menu_state.menu()
-                self.menu_state.draw(self.screen)
+                self.state_menu()
+
             if self.state == States.EXIT:
-                self.QUIT = True
+                self.EXIT = True
 
             pygame.display.update()
             self.clock.tick(60)
--- pause_state.py
+++ pause_state.py
@@ -1,5 +1,7 @@
 import pygame
 from pause_sprites import Background, PausePanel, PauseLogo
+from menu_sprites import ExitButtonSprite
+from menu_state import States
 
 
 class Pause:
@@ -7,6 +9,7 @@ class Pause:
         self.background = Background(screen_width, screen_height)
         self.pause_panel = PausePanel(screen_width, screen_height)
         self.pause_logo = PauseLogo(screen_width, screen_height)
+        self.exit_button = ExitButtonSprite(screen_width, screen_height)
 
         self.background_sprites_list = pygame.sprite.Group()
         self.background_sprites_list.add(self.background)
@@ -14,12 +17,27 @@ class Pause:
         self.panel_sprite_list = pygame.sprite.Group()
         self.panel_sprite_list.add(self.pause_panel)
 
-        self.logo_sprite_list = pygame.sprite.Group()
-        self.logo_sprite_list.add(self.pause_logo)
+        self.pause_sprite_list = pygame.sprite.Group()
+        self.pause_sprite_list.add(self.pause_logo, self.exit_button)
+        self.new_state = States.PAUSE
+
+    def update(self):
+        mouse_position = pygame.mouse.get_pos()
+        pressed = pygame.mouse.get_pressed()
+
+        self.exit_button.set_state(0)
+
+        if self.exit_button.rect.collidepoint(mouse_position):
+            if pressed[0]:
+                self.new_state = States.EXIT
+            self.exit_button.set_state(1)
+
+        self.pause_sprite_list.update()
+        return self.new_state
 
     def draw(self, screen):
         self.background_sprites_list.draw(screen)
         self.panel_sprite_list.draw(screen)
-        self.logo_sprite_list.draw(screen)
+        self.pause_sprite_list.draw(screen)
 
 
